d1a24d1102a1fe43fc2abde2221b74cfee175fb8
mert arın <mertkarin2@gmail.com>
2024-01-02 19:02:19 -0600

    yandabiseyleracikkencalisnoktakom

--- boyledegil/kma01.java
+++ boyledegil/kma01.java
@@ -1,7 +1,13 @@
 package boyledegil;
 
+import java.util.Scanner;
+
 public class kma01 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        scanner.close();
         
+
     }
 }
--- ilhanjavjav.java
+++ ilhanjavjav.java
@@ -1,1 +1,28 @@
 // buralarda java'yı yeniden hatırlıyor olacağım..
+
+public class ilhanjavjav {
+    public static void main(String[] args) {
+
+        System.out.println(Integer.MAX_VALUE); //2147483647
+        System.out.println(Integer.MIN_VALUE); //-2147483648
+        System.out.println(Short.MAX_VALUE); //32767
+        System.out.println(Short.MIN_VALUE); //-32768
+
+        System.out.println(Character.MAX_VALUE); //
+        System.out.println(Integer.MIN_VALUE); //2147483648
+        System.out.println(Integer.MAX_VALUE); //2147483647
+        System.out.println(Character.MAX_VALUE); //
+        System.out.println(Character.MIN_VALUE); //
+        System.out.println(Short.MIN_VALUE); //-32768
+        System.out.println(Short.MAX_VALUE); //32767
+        System.out.println(Long.MIN_VALUE); //-9223372036854775808
+        System.out.println(Long.MAX_VALUE); //9223372036854775807
+        System.out.println(Double.MAX_VALUE); //1.7976931348623157E308
+        System.out.println(Double.MIN_VALUE); //4.9E-324
+        System.out.println(Byte.MIN_VALUE); //-128
+        System.out.println(Byte.MAX_VALUE); //127
+        System.out.println(Float.MIN_VALUE); // 1.4E-45
+        System.out.println(Float.MAX_VALUE); //3.4028235E38
+    
+}
+}
